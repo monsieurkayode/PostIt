b0046e3e23fef4fe15d3ae19a39be9feb535824d
Kayode Adeola <monsieurkayode@gmail.com>
2017-07-14 00:41:46 +0100

    add test for user

---
+++ server/test/test.js
@@ -0,0 +1,46 @@
+import supertest from 'supertest';
+import mocha from 'mocha';
+import app from '../../app';
+import models from '../models';
+import { should, expect, assert } from 'chai';
+
+const server = supertest.agent(app);
+
+const jane = {
+  username: 'janeDoe',
+  password: 'password',
+  email: 'janeDoe@gmail.com'
+};
+
+describe('User Registration', () => {
+  it('allows a new user to register', () => {
+    server
+      .post('/api/user/signup')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(jane)
+      .expect(201)
+      .end((err, res) => {
+        res.status.should.equal(201);
+        res.body.success.should.equal(true);
+        done();
+      });
+  });
+
+describe('User Login', () => {
+  it('allows a new user to register', () => {
+    server
+      .post('/api/user/signin')
+      .set('Connection', 'keep alive')
+      .set('Content-Type', 'application/json')
+      .type('form')
+      .send(jane)
+      .expect(200)
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.success.should.equal(true);
+        done();
+      });
+  });
+
