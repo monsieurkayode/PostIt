2a286a91df7ede1c8e6e77a58dcb32dea701c342
Kayode Adeola <monsieurkayode@gmail.com>
2017-07-21 15:42:34 +0100

    add token

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -14,12 +14,12 @@ const secret = process.env.secretKey;
 const login = {
   signin(req, res) {
     if (!req.body.username) {
-      return res.status(400).json({
+      return res.send({
         error: 'Username must not be empty'
       });
     }
     if (!req.body.password) {
-      return res.status(400).json({
+      return res.send({
         error: 'Password must not be empty'
       });
     }
@@ -27,13 +27,14 @@ const login = {
       .findOne({ where: { username: req.body.username } })
       .then((user) => {
         if (!user) {
-          return res.status(401).json({ message: 'Invalid Auth Details' });
+          return res.send({ message: 'Invalid Authentication Details' });
         }
         const check = bcrypt.compareSync(req.body.password, user.password);
         if (check) {
           const token = jwt.sign({ user }, secret);
-          res.status(200).json({
-            message: `Welcome to PostIt ${user.username}`,
+          res.send({
+            message: 'Success, Token successfully generated',
+            Token: token
           })
             .catch(error => res.status(404).send(error));
         }
